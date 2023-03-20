cc3c10cf78d9c0c28b619975bf31addeb40eb8f6
joblas <blasj408@gmail.com>
2023-03-19 21:08:02 -0700

    fixed grammer

--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -81,7 +81,7 @@ const CreatePost = () => {
     <section className="max-w-7xl mx-auto">
       <div>
         <h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Below enter your name then add some descriptive prompts and let DALL-E AI do magic. Share it with the community when satified with the results</p>
+        <p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">Below enter your name then add some descriptive prompts and let DALL-E AI do magic. Share it with the community when satisfied with the image</p>
       </div>
 
       <form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
