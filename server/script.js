081c9cf2e67f64f000cda20932894db4b10b9141
joblas <blasj408@gmail.com>
2023-07-05 13:33:18 -0700

    fixed f_webp to just webp

--- server/script.js
+++ server/script.js
@@ -5,7 +5,7 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
-  fetch_format: 'f_webp',
+  fetch_format: 'webp',
   quality: 'auto',
 });
 
