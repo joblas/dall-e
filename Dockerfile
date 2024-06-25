91f30d1d3ad14a7b5f3a57aba6e5b369db5265a7
joe <blasj@gmail.com>
2024-06-24 20:50:54 -0700

    update index.js process.env.PORT

--- Dockerfile
+++ Dockerfile
@@ -14,9 +14,5 @@ COPY . .
 # Expose the port your app runs on
 EXPOSE 8080
 
-# Set the environment variable
-ENV OPENAI_API_KEY=your_api_key_here
-ENV MongoDB_URI=your_mongodb_uri_here
-
 # Run the application
 CMD ["npm", "start"]
--- server/index.js
+++ server/index.js
@@ -17,14 +17,15 @@ app.use('/api/v1/dalle', dalleRoutes);
 
 app.get('/', async (req, res) => {
   res.status(200).json({
-    message: 'Hello funboi from DALL.E! ON Render.com server',
+    message: 'Hello funboi from DALL.E! server',
   });
 });
 
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server has started on port http://localhost:8080'));
+    await connectDB(process.env.MONGODB_URL);
+    const port = process.env.PORT || 8080;
+    app.listen(port, () => console.log(`Server has started on port http://localhost:${port}`));
   } catch (error) {
     console.log(error);
   }
